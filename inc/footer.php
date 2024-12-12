e40e93ab8b863231dfa6e2312df5dcf30566afb3
luissfernando <lmoralest@unsa.edu.pe>
2024-12-11 22:16:46 -0500

    ProyectoDB1

---
+++ inc/footer.php
@@ -0,0 +1,16 @@
+<footer>
+    <h3 class="text-center">Siguenos en</h3><br>
+    <ul class="list-unstyled text-center">
+        <a href="#" class="social-icon all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="Facebook">
+            <img src="assets/icons/social-facebook.png" alt="facebook-icon">
+
+       
+       
+        </a>
+        <a href="#" class="social-icon all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="Twitter">
+            <img src="assets/icons/social-twitter.png" alt="twitter-icon">
+        </a>
+    </ul>
+    <br><br><br>
+    <h5 class="text-center tittles-pages-logo">AQPELUCHES &copy; 2018</h5>
+</footer>
